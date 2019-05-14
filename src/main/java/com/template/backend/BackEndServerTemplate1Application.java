6812b4821122260d1bb1d14be0af39958f25d7e5
jewook89 <jewook89@gmail.com>
2019-05-15 00:19:36 +0900

    Add BackEndtemplate Test Code

---
+++ src/main/java/com/template/backend/BackEndServerTemplate1Application.java
@@ -0,0 +1,13 @@
+package com.template.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndServerTemplate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndServerTemplate1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/template/backend/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.template.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	String index() {
+		return "hello";
+	}
+	
+}
